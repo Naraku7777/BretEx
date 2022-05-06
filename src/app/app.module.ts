8333c1f71e7eab8e3d30cf72eb338cf7554a8bd0
Naraku7777 <vargasdaniel195@gmail.com>
2022-05-05 21:50:44 -0600

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppMaterialModule } from './shared/app-material.module';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppRoutingModule } from './app-routing.module';
-
 
 import { environment } from './environment/environment';
 import { AngularFireModule } from '@angular/fire/compat';
@@ -19,8 +17,10 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { Page1Component } from './Pages/page1/page1.component';
 import { Page2Component } from './Pages/page2/page2.component';
 
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
 @NgModule({
-  imports:      [ AppMaterialModule, FlexLayoutModule, BrowserModule, AppRoutingModule, FormsModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule],
+  imports:      [ AngularFireDatabaseModule, AppMaterialModule, FlexLayoutModule, BrowserModule, AppRoutingModule, FormsModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule],
   declarations: [ AppComponent, HelloComponent, NavbarComponent, Page1Component, Page2Component],
   bootstrap:    [ AppComponent ]
 })
