7f2e78b0e76470fbc5f994fbbfc5573b282a3e48
Naraku7777 <vargasdaniel195@gmail.com>
2022-05-05 22:50:41 -0600

    5 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { Page1Component } from './Pages/page1/page1.component';
 import { Page2Component } from './Pages/page2/page2.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from './environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/public_api';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppMaterialModule, FlexLayoutModule, AppRoutingModule ],
+  imports:      [ BrowserModule, FormsModule, AppMaterialModule, FlexLayoutModule, AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFirestoreModule],
   declarations: [ AppComponent, HelloComponent, NavbarComponent, Page1Component, Page2Component ],
   bootstrap:    [ AppComponent ]
 })
